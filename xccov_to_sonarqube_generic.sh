abb4df5266554561e4d16bf588964848f5cd44cf
MMarkoUp <139963226+MMarkoUp@users.noreply.github.com>
2024-11-05 13:31:23 +0100

    Create xccov_to_sonarqube_generic.sh

---
+++ xccov_to_sonarqube_generic.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Source of the script
+# https://github.com/SonarSource/sonar-scanning-examples/blob/master/swift-coverage/swift-coverage-example/xccov-to-sonarqube-generic.sh
+
+function convert_xccov_to_xml {
+    sed -n \
+        -e '/:$/s/&/\&amp;/g;s/^\(.*\):$/  <file path="\1">/p' \
+        -e 's/^ *\([0-9][0-9]*\): 0.*$/    <lineToCover lineNumber="\1" covered="false"\/>/p' \
+        -e 's/^ *\([0-9][0-9]*\): [1-9].*$/    <lineToCover lineNumber="\1" covered="true"\/>/p' \
+        -e 's/^$/  <\/file>/p'
+}
+
+function xccov_to_generic {
+    local xcresult="$1"
+
+    echo '<coverage version="1">'
+    xcrun xccov view --archive "$xcresult" | convert_xccov_to_xml
+    echo '</coverage>'
+}
+
+function check_xcode_version() {
+    local major=${1:-0} minor=${2:-0}
+    return $(((major >= 14) || (major == 13 && minor >= 3)))
+}
+
+if ! xcode_version="$(xcodebuild -version | sed -n '1s/^Xcode \([0-9.]*\)$/\1/p')"; then
+    echo 'Failed to get Xcode version' 1>&2
+    exit 1
+elif check_xcode_version ${xcode_version//./ }; then
+    echo "Xcode version '$xcode_version' not supported, version 13.3 or above is required" 1>&2
+    exit 1
+fi
+
+xcresult=$BITRISE_XCRESULT_PATH
+if [[ ! -d $xcresult ]]; then
+    echo "Path not found: $xcresult" 1>&2
+    exit 1
+elif [[ $xcresult != *".xcresult"* ]]; then
+    echo "Expecting input to match '*.xcresult', got: $xcresult" 1>&2
+    exit 1
+fi
+
+xccov_to_generic "$xcresult" > $BITRISE_PROJECT_PATH/sonarqube-generic-coverage.xml
